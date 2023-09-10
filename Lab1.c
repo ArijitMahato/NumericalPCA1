5d0de9aa5383d188002fbdee9e6bd752f8cc0b39
Arijit Mahato <mahatoarijit03@gmail.com>
2023-09-10 17:57:13 +0530

    changes saved

---
+++ Lab1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    float r1, r2;
+ float a, b, c, k;
+ printf("Enter the coefficients a , b, c restpectively: ");
+ scanf("%f%f%f", &a, &b, &c);
+ 
+ k = sqrt (b*b - 4*a*c);
+
+ printf("First root is: ");
+
+
+ 
+
+
+
+    return 0;
+
+}
